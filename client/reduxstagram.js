35fcc7a57aab3aa42560f649ca8453592de19c43
srdjanRakic <srg_ian@hotmail.com>
2017-11-13 15:42:09 +0100

    Add App component and connect the store.

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -5,7 +5,26 @@ import { render } from 'react-dom';
 import css from './styles/style.styl';
 
 // Import Components
-import Main from './components/Main';
+import App from './components/App';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main><p>Hello</p></Main>,
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
+const router = (
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={App}>
+                <IndexRoute component={PhotoGrid}></IndexRoute>
+                <Route path="/view/:postId" component={Single}></Route>
+            </Route>
+        </Router>
+    </Provider>
+)
+
+
+render(router,
     document.getElementById('root'));
