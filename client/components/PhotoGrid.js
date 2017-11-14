44c7e2271d99ef92e4dbbeaed13fc700b345db48
srdjanRakic <srg_ian@hotmail.com>
2017-11-14 14:12:20 +0100

    Show posts in PhotoGrid component.

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
     render() {
         return (
             <div className="photo-grid">
-                I am the photo grid.
+               {this.props.posts.map((post, i) => <Photo key={i} i={i} post={post} {...this.props} />)}
             </div>
         )
     }
