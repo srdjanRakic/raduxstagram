a3ec7c641925addf267ec16faa9c8f8805ff109a
srdjanRakic <srg_ian@hotmail.com>
2017-11-14 15:46:07 +0100

    Setup Sentry for tracking exceptions.

--- client/data/config.js
+++ client/data/config.js
@@ -1,7 +1,7 @@
 import Raven from 'raven-js';
 
-const sentry_key = 'cb55d4f05cd443ce82303222f77ef5e0';
-const sentry_app = '61499';
+const sentry_key = '3c7a27e23d904ba587cf4386745c11ea';
+const sentry_app = '244993';
 export const sentry_url = `https://${sentry_key}@app.getsentry.com/${sentry_app}`;
 
 export function logException(ex, context) {
