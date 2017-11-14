e2fd96ecf9b3637d01821dfc4881f57ce1d58f9f
srdjanRakic <srg_ian@hotmail.com>
2017-11-14 14:11:50 +0100

    Add Posts reducer.

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,6 +1,17 @@
 function posts(state = [], action) {
-    console.log("The post will change.");
-    console.log(state, action);
+    const { type, index } = action;
+    switch(type) {
+        case 'INCREMENT_LIKES':
+            console.log("incrementing likes!");
+            return [
+                ...state.slice(0, index), // before the one we are updating
+                {...state[index], likes: state[index].likes + 1 },
+                ...state.slice(index + 1), // after the one we are updating
+            ];
+        default:
+            return state;
+    }
+
     return state;
 }
 
